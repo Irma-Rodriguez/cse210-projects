bbecfc5ec96633a52a6299e25caba343f7fbcf5a
irodriguezcardenas@byupathway.edu <irodriguezcardenas@byupathway.edu>
2025-11-12 22:40:24 -0500

    structure of my proyect

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,27 @@
+using System;
+
+public class Scripture
+{
+    private Reference _Reference;
+    private List<Word> _words;
+
+    public Scripture(Reference references, string text)
+    {
+
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+
+    }
+
+    public string GetDisplayText()
+    {
+        return "";
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        return false;
+    }
+}
