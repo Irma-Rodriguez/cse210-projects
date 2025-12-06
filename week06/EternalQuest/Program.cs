0d50f34e9febf1d63fbfec8116aaf43b6d54ed2b
irodriguezcardenas@byupathway.edu <irodriguezcardenas@byupathway.edu>
2025-12-06 14:53:59 -0500

    Submitting my assingment

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -1,9 +1,10 @@
+// I added a feature that allows the player to increase his level every 500 points, when the player levels up, the game displays a congratulations message.
 using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the EternalQuest Project.");
+        GoalManager manager = new GoalManager();
+        manager.Start();
     }
 }
